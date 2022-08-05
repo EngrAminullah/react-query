0c6c6abe60da5720cb85c83d8bc9a84d5d85bf30
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-08-05 11:56:49 +0500

    dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider, Heading } from "@chakra-ui/react";
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-function App() {
+const App = () => {
+  const queryClient = new QueryClient();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <Heading>Hello React-query</Heading>
+      </QueryClientProvider>
+    </ChakraProvider>
   );
-}
+};
 
 export default App;
