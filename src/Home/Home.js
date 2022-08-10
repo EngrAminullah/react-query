1021f35d9266b8bd8d0f572937b69716827bc5d1
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-08-10 13:44:35 +0500

    API error

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,18 +2,17 @@ import React from "react";
 import { useQuery } from "react-query";
 import axios from "axios";
 const fetchPosts = async () => {
-  try {
-    const { data } = await axios.get("https://gorest.co.in/public/v2/posts");
-    console.log(data, "bacha");
-    return data;
-  } catch (error) {
-    throw Error("Unable to fetch posts");
-  }
+  // let data = await fetc("https://jsonplaceholder.typicode.com/todos/1");
+  let data = await axios.get("https://gorest.co.in/public/v2/posts");
+
+  console.log(data.data, "bacha");
+  return data.data;
 };
 const Home = () => {
-  const { data, isLoading, error } = useQuery("posts", fetchPosts);
-  console.log(error, "jjjjjjjj");
+  let data = useQuery("posts", fetchPosts);
+
+  console.log(data, "jjjjjjjj");
   return <div>Home</div>;
 };
 
-export default Home;
+export default fetchPosts;
