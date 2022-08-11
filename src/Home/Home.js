2b6053f287cd087b5e279b8766fc8c1787717b9f
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-08-11 15:18:22 +0500

    asd

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useQuery } from "react-query";
 import axios from "axios";
 const fetchPosts = async () => {
-  // let data = await fetc("https://jsonplaceholder.typicode.com/todos/1");
-  let data = await axios.get("https://gorest.co.in/public/v2/posts");
+  try {
+    // let data = await fetc("https://jsonplaceholder.typicode.com/todos/1");
+    let { data } = await axios.get("https://gorest.co.in/public/v2/posts", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
 
-  console.log(data.data, "bacha");
-  return data.data;
+    console.log(data, "bacha");
+    return data;
+  } catch (error) {
+    throw Error("Unable to fetch posts");
+  }
 };
 const Home = () => {
-  let data = useQuery("posts", fetchPosts);
+  // const { data, isLoading, error } = useQuery("posts", fetchPosts);
 
-  console.log(data, "jjjjjjjj");
+  // console.log(data, "jjjjjjjj");
+  useEffect(() => {
+    fetchPosts();
+  }, []);
   return <div>Home</div>;
 };
 
-export default fetchPosts;
+export default Home;
